5f1eaeba5260d512b054826042f6053f1e468fcd
Shashikiran Neelakantaiah <shashikiran640@gmail.com>
2023-02-07 18:53:37 +0530

    Completed till Media folder, Added default profile picture in Models

--- social_book/core/models.py
+++ social_book/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    user = ""
+    id_user = ""
+    bio = ""
+    profileimg = models.ImageField(upload_to='profile_imagesś', default='default-profile-picture.png')
+    location = ""
