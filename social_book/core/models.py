8998f8b0444a9eaa6513d8690637696bae375119
Shashikiran Neelakantaiah <shashikiran640@gmail.com>
2023-02-09 15:59:28 +0530

    Added SignUp view, added static files, added Users and Profiles

--- social_book/core/models.py
+++ social_book/core/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 # Create your models here.
 class Profile(models.Model):
-    user = ""
-    id_user = ""
-    bio = ""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
     profileimg = models.ImageField(upload_to='profile_imagesś', default='default-profile-picture.png')
-    location = ""
+    location = models.CharField(max_length=100, blank=True)
+
+
+    def __str__(self) -> str:
+        return self.user.username
--- social_book/core/urls.py
+++ social_book/core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# Add Path to direct to views
 urlpatterns = [
-    path('', view=views.index, name="index")
+    path('', view=views.index, name="index"),
+    path('signup', view=views.signup, name="signup")
 ]
